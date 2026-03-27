4e999b4faba813ea190c014a80d389b6e053979a
garymurasira <g.murasira@alustudent.com>
2026-03-27 20:26:55 +0200

    done

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """Function that queries Reddit API and returns number of subscribers"""
-import urllib.request
 import json
+import urllib.request
 
 
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     req = urllib.request.Request(url)
-    req.add_header('User-Agent', 'linux:myredditapp:v1.0 (by /u/GabbyIT-pixel)')
+    req.add_header(
+        'User-Agent', 'linux:myredditapp:v1.0 (by /u/GabbyIT-pixel)'
+    )
     try:
-        response = urllib.request.urlopen(req)
+        response = urllib.request.urlopen(req, timeout=10)
         data = json.loads(response.read().decode('utf-8'))
         return data.get("data", {}).get("subscribers", 0)
     except Exception:
