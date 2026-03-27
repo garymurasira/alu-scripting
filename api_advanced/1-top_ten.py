849521ff232138bf47205421752a790613b4b168
garymurasira <g.murasira@alustudent.com>
2026-03-27 20:39:18 +0200

    done

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-""" top_ten.py """
-import requests
+"""Function that queries Reddit API and prints top 10 hot posts"""
+import json
+import urllib.error
+import urllib.request
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    """Prints the titles of the first 10 hot posts for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    req = urllib.request.Request(url)
+    req.add_header(
+        'User-Agent', 'linux:myredditapp:v1.0 (by /u/GabbyIT-pixel)'
+    )
+    try:
+        response = urllib.request.urlopen(req, timeout=10)
+        data = json.loads(response.read().decode('utf-8'))
+        posts = data.get("data", {}).get("children", [])
+        for post in posts:
+            print(post.get("data", {}).get("title"))
+    except urllib.error.HTTPError:
         print(None)
-        return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
+    except urllib.error.URLError:
+        print(None)
