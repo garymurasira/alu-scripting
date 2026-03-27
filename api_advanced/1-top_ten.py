3dd9845c313e3d3a5505c2482397c0dfb3b421ac
garymurasira <g.murasira@alustudent.com>
2026-03-27 20:30:00 +0200

    done

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """Function that queries Reddit API and prints top 10 hot posts"""
-import urllib.request
 import json
+import urllib.request
 
 
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts for a given subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     req = urllib.request.Request(url)
-    req.add_header('User-Agent', 'linux:myredditapp:v1.0 (by /u/GabbyIT-pixel)')
+    req.add_header(
+        'User-Agent', 'linux:myredditapp:v1.0 (by /u/GabbyIT-pixel)'
+    )
     try:
-        response = urllib.request.urlopen(req)
+        response = urllib.request.urlopen(req, timeout=10)
         data = json.loads(response.read().decode('utf-8'))
         posts = data.get("data", {}).get("children", [])
         for post in posts:
